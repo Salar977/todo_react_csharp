684335d20d41562dc2990acac665e05b7c19e017
Salar Amir <salar831@gmail.com>
2025-10-27 18:38:58 +0100

    First api endpoint

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,35 @@
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql(connectionString);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async ([FromServices] MyDbContext dbContext) =>
+{
+    var myTodo = new Todo()
+    {
+        Id = Guid.NewGuid().ToString(),
+        Title = "test title",
+        Description = "test description",
+        Priority = 5,
+        Isdone = false
+    };
+    await dbContext.AddAsync(myTodo);
+    await dbContext.SaveChangesAsync();
+    
+    var objects = dbContext.Todos.ToList();
+    return Results.Ok(objects);
+});
 
 app.Run();
